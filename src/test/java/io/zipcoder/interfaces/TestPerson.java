04ebdc5cc07292ed700cab262a66c84e39d2e1ac
Alyssa Resh <aresh@Zipcoders-MacBook-Pro.local>
2024-12-26 23:24:11 -0500

    person tests complete

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -19,17 +19,27 @@ public class TestPerson {
 //            -
     @Test
     public void testConstructor(){
+//        ### Part 1.0 - Test `Person`
+//        Create a `TestPerson` class.
+//        * Create a `testConstructor` method which ensures that a `Person`
+//        object's `id` and `name` field are being set upon construction.
+
         //given
         Person person = new Person(123456, "Joe");
         assertEquals(123456, person.getId());
         assertEquals("Joe", person.getName());
     }
 
-//            ### Part 1.0 - Test `Person`
-//            * Create a `TestPerson` class.
-//            * Create a `testConstructor` method which ensures that a `Person`
-//            object's `id` and `name` field are being set upon construction.
 //            * Create a `testSetName` method which ensures that a `Person`
 //            object's `name` variable is being set by invoking the `.setName` method.
+    @Test
+    public void testSetName(){
+        //given
+        Person person = new Person(123456, "Joe");
+        //when
+        person.setName("Dash");
+        //expected
+        assertEquals("Dash", person.getName());
+    }
 
 }
